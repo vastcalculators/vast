43f4a837964ff8175bddd44b81f48ae03f80bb6e
vastcalculators <vastcalc@gmail.com>
2026-01-08 11:59:34 +0500

    google-site-verification-in-public

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -71,7 +71,7 @@ export default async function LocaleLayout({ children, params }: LayoutProps) {
 
   return (
     <html lang={htmlLang} dir={isRTL ? "rtl" : "ltr"} suppressHydrationWarning>
-    <meta name="google-site-verification" content="XZo-Tyhr0oPTXceVtmo0IODQJMJRPTJRBzBE6eLRND8" />
+    
 
       <Script
         async
